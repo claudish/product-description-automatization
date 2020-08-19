9e32757a9c8049013d866f7caaf0312ea29d5388
claudish <57326763+claudish@users.noreply.github.com>
2020-08-19 21:55:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ var sectionsCounter = 1;
 
 
 function convertText() {
-    document.getElementById("textarea").innerHTML = "";
-    for (var i = 1; i <= sectionsCounter; i++) {
-        var sec = document.getElementById("section-" + i);
-        convertSection(sec);
-    }
+  document.getElementById("textarea").innerHTML = "";
+  for (var i = 1; i <= sectionsCounter; i++) {
+    var sec = document.getElementById("section-" + i);
+    convertSection(sec);
+  }
 }
 
 function convertSection(section) {
-    var header = section.getElementsByClassName("header")[0];
+  var header = section.getElementsByClassName("header")[0];
   var h2 = document.createElement("h2");
   h2.textContent = header.value;
   console.log(h2.innerHTML);
@@ -29,19 +29,26 @@ var ul = document.createElement("ul");
               var lielement = document.createElement("li");       
                   lielement.textContent = li;
 ul.appendChild(lielement);
+                     
+                   }
+                })
+document.getElementById("textarea").innerHTML += ul.outerHTML;  
+  var image = section.getElementsByClassName("image")[0];
+var img = document.createElement("img");
+img.src = image.value;
+document.getElementById("textarea").innerHTML += img.outerHTML; 
 }
 
 
 function addMore() {
-    var sections = document.getElementById("sections");
-    var section1Clone = document.getElementById("section-1").cloneNode(true);
-    section1Clone.id = "section-" + ++sectionsCounter;
-    
+  var sections = document.getElementById("sections");
+  var section1Clone = document.getElementById("section-1").cloneNode(true);
+  section1Clone.id="section-" + ++sectionsCounter;
+  sections.appendChild(section1Clone);
+var input = section1Clone.getElementsByTagName("input");
+  input[0].value ="";
+    input[1].value ="";
+    input[2].value ="";
 
-    sections.appendChild(section1Clone);
-    var input = section1Clone.getElementsByTagName("input");
-
-    input[0].value = "";
-    input[1].value = "";
-    input[2].value = "";
 }
+
